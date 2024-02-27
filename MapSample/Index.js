bbc578f7a22a71961feffd1003a480aa045790e0
Breno P. Lucena <breno.lucena@live.com>
2024-02-27 12:58:54 +0000

    Create Index.js

---
+++ MapSample/Index.js
@@ -0,0 +1,9 @@
+let arr = [
+  { id: 1, age: 12, name: 'Manu' },
+  { id: 2, age: 24, name: 'Quincy' },
+  { id: 3, age: 22, name: 'Abbey' },
+]
+
+let names = arr.map((el) => el.name)
+console.log(names)
+// Output: [ 'Manu', 'Quincy', 'Abbey' ]
